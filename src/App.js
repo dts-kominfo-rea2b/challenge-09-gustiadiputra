ed7d6f621eeafaf8ad8df4e90648a5b47b3d654c
Gusti Adi Putra <gustiadi.putra20@gmail.com>
2022-06-27 10:39:51 +0700

    revisi2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import Header from "./components/Header";
-import Contact from "./components/Contact";
+import './App.js';
+import Header from './components/Header';
+import Contact from './components/Contact';
 
 // Uncomment untuk memuat daftar kontak
 import contacts from "./data/contacts.json";
